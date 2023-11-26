970b1f7bbbb2e82fee09004ad26f5b1ace4cfd1a
LokKruDaro <lokkrudaro@gmail.com>
2023-11-26 17:28:12 +0700

    Add routing

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,4 @@
+const SignUp = () => {
+  return <div>sign up</div>;
+};
+export default SignUp;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">next js is 13 is comming</h1>
-      <h2 className="h2-bold">next js is 13 is comming</h2>
-      <h3 className="h3-bold">next js is 13 is comming</h3>
-    </div>
-  );
-}
